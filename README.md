96d80e1107b7b6c8e7b41902c15bfccfc476fda8
joacoesperon <joacoesperon1@gmail.com>
2025-02-16 22:36:35 +0100

    update

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Joaquin Esperon Trading Bot Repository
 
+# RBI FrameWork
+    
+    RBI - Research, Backtest, Implement
+    
+    Research: research trading strategies and alpha generation techniques (google scholar, books, podcasts, youtube)
+    Backtest: use ohlcv data in order to backtest the strategy
+    Implement: if the backtest is profitable, implement into a trading bot with tiny size & scale slowly
+
+# Useful tips
+
+    para instalar todas las dependencias: pip install -r requirements.txt
+    para desisntalar todas las dependencias: pip uninstall -r requirements.txt -y
--- aux_funcs.py
+++ aux_funcs.py
@@ -12,7 +12,7 @@ import schedule
 import requests
 from datetime import datetime, timedelta
 import pandas_ta as ta
-prints('ONLY WORKS ON 3.8.5 QUANT INTERPRETER')
+# print('ONLY WORKS ON 3.8.5 QUANT INTERPRETER')
 
 symbol = 'BTCUSD'
 timeframe = '15m'
@@ -59,10 +59,85 @@ def process_data_to_df(snapshot_data):
 
         return df
     else:
-        return pd.DataFrame() # return empty dataframe if no data is fetched
+        return pd.DataFrame() # return empty dataframe if no data is fetched    
+
+def supply_demand_zones_hl(symbo, timeframe, limit):
+
+    print('starting joacos supply and demand zone calculations...')
+
+    sd_df
+
+    df['supp_lo'] = df[:-2]['low'].min()
+    supp_lo = df.iloc[-1]['supp_lo']
+
+    df['res_hi'] = df[:-2]['high'].max()
+    res_hi = df.iloc[-1]['res_hi']
+
+    print(df)
+
+
+    sd_df[f'{timeframe}_dz'] = [supp_lo, supp]
+    sd_df[f'{timeframe}_sz'] = [res_hi, resis]
+
+    print('here are joaco supply and demand zones')
+    print(sd_df)
+
+    return sd_df
+
+def get_position(symbol,account):
+
+    """
+    gets the current position info, like size, etc.
+    """
+
+    #account = LocalAccount = eth_account.Account.from_key(key)
+    info = Info(constants.MAINNET_API_URL, skip_ws=True)
+    user_state = info.user_state(account.address)
+    print(f'this is current account value: {user_state["marginSummary"]["accountValue"]}')
+    positions = []
+    print(f'this is the symbol {symbol}')
+    print(user_state["assetPositions"])
+    for position in user_state["assetPositions"]:
+        if (position["position"]["coin"] == symbol) and float(position["position"]["szi"]) != 0:
+            positions.append(position["position"])
+            in_pos = True
+            size = float(position["position"]["szi"])
+            pos_sym = position["position"]["coin"]
+            entry_px = float(position["position"]["entryPx"])
+            pnl_perc = float(position["position"]["returnOnEquity"])*100
+            print(f'this is the pnl perc {pnl_perc}')
+            break
+    else:
+        in_pos = False
+        size = 0
+        pos_sym = None
+        entry_px = 0
+        pnl_perc = 0
+    
+    if size > 0:
+        long = True
+    elif size < 0:
+        long = False
+    else:
+        long = None
+
+
+
+    
+
+def create_combined_df(ob, exchange):
+    bids_df = pd.DataFrame(ob['bids'], columns=['Bid', 'Bid Size'])
+    asks_df = pd.DataFrame(ob['asks'], columns=['Ask', 'Ask Size'])
+    if exchange == 'coinbasepro' and len(bids_df) > 100:
+        bids_df = bids_df.head(100)
+        asks_df = asks_df.head(100)
+    return pd.concat([asks_df.reset_index(drop=True), bids_df.reset_index(drop=True)], axis=1)
+
 
 def find_max_sizes(df):
-    
+    max_bid_size_row = df.loc[df['Bid Size'].idxmax()]
+    max_ask_size_row = df.loc[df['Ask Size'].idxmax()]
+    return max_bid_size_row['Bid'], max_bid_size_row['Bid Size'], max_ask_size_row['Ask'], max_ask_size_row['Ask Size']
 
 def find_before_biggest(df,max_price, col_name, is_bid=True):
     # print(f'finding before biggest for {col_name}')
@@ -85,7 +160,10 @@ def find_before_biggest(df,max_price, col_name, is_bid=True):
 
 
 
-# funciones para get_data
+# funciones para get_data.py
+import os
+import re
+
 
 def history_file_name(index, exchange, symbol):
     """
